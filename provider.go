5ab19904ced65f91fd06d8998408869a5f1bdf34
William Campoli <william.campoli@logimethods.com>
2019-06-13 09:33:35 -0400

    first commit

---
+++ provider.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+
+		Schema: map[string]*schema.Schema{
+			"api_url": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("DT_API_BASE_URL", ""),
+			},
+			"api_token": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("DT_API_TOKEN", ""),
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"dynatrace_user": resourceUser(),
+		},
+
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+
+	config := Config{
+		ClusterUrl: d.Get("api_url").(string),
+		ApiToken:   d.Get("api_token").(string),
+	}
+
+	return &config, nil
+}
---
+++ resource_user.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dtcookie/dynatrace/apis/onprem/users"
+	"github.com/dtcookie/dynatrace/rest"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceUser() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceUserCreate,
+		Read:   resourceUserRead,
+		Update: resourceUserUpdate,
+		Delete: resourceUserDelete,
+
+		Schema: map[string]*schema.Schema{
+			"user_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"first_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"last_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"email": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"user_groups": &schema.Schema{
+				Type: schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceUserCreate(d *schema.ResourceData, m interface{}) error {
+	config := m.(*Config)
+
+	api := users.NewAPI(&rest.Config{Insecure: true, NoProxy: false},
+		rest.NewCredentials(config.ClusterUrl, config.ApiToken))
+
+	userConfig := getUserConfig(d)
+
+	_, err := api.Create(userConfig)
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(d.Get("user_id").(string))
+	return resourceUserRead(d, m)
+}
+
+func resourceUserRead(d *schema.ResourceData, m interface{}) error {
+	config := m.(*Config)
+
+	api := users.NewAPI(&rest.Config{Insecure: true, NoProxy: false},
+		rest.NewCredentials(config.ClusterUrl, config.ApiToken))
+
+	_, err := api.GetUser(d.Get("user_id").(string))
+	if err != nil {
+		d.SetId("")
+		return err
+	}
+
+	return nil
+}
+
+func resourceUserUpdate(d *schema.ResourceData, m interface{}) error {
+	config := m.(*Config)
+
+	api := users.NewAPI(&rest.Config{Insecure: true, NoProxy: false},
+		rest.NewCredentials(config.ClusterUrl, config.ApiToken))
+
+	userConfig := getUserConfig(d)
+
+	_, err := api.Update(userConfig)
+	if err != nil {
+		return err
+	}
+
+	return resourceUserRead(d, m)
+}
+
+func resourceUserDelete(d *schema.ResourceData, m interface{}) error {
+	config := m.(*Config)
+
+	api := users.NewAPI(&rest.Config{Insecure: true, NoProxy: false},
+		rest.NewCredentials(config.ClusterUrl, config.ApiToken))
+
+	_, err := api.Delete(d.Get("user_id").(string))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getUserConfig(d *schema.ResourceData) *users.UserConfig {
+	groupsInterface := d.Get("user_groups").([]interface{})
+
+	groups := make([]string, len(groupsInterface))
+
+	for i, group := range groupsInterface {
+		groups[i] = fmt.Sprint(group)
+	}
+
+	userConfig := users.UserConfig{
+		ID:        d.Get("user_id").(string),
+		EMail:     d.Get("email").(string),
+		FirstName: d.Get("first_name").(string),
+		LastName:  d.Get("last_name").(string),
+		Groups:    groups,
+	}
+
+	return &userConfig
+}
